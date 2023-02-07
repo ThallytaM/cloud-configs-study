1589cc85e95bfb3cbdf944a6ccce109e6c7bd02b
Thallyta <thallytamedeiros2011@gmail.com>
2023-02-07 13:49:15 -0300

    Atualizaçao config

--- ms-config-server/ms-config-server/src/main/java/com/cloudstudy/msconfigserver/MsConfigServerApplication.java
+++ ms-config-server/ms-config-server/src/main/java/com/cloudstudy/msconfigserver/MsConfigServerApplication.java
@@ -2,6 +2,7 @@ package com.cloudstudy.msconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
